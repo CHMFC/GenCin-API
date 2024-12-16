a1474e0d3c38c1477a9d2ace7c63a04ed143940f
CHMFC <chmfc@cin.ufpe.br>
2024-12-16 15:34:59 -0300

    Primeiro commit

---
+++ src/main/java/com/GenCin/GenCin/GenCinApplication.java
@@ -0,0 +1,13 @@
+package com.GenCin.GenCin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenCinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenCinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GenCin/GenCin/GenCinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GenCin.GenCin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenCinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
