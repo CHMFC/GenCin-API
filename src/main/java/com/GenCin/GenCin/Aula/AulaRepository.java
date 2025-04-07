07130edc9564814552ce4e107c1d3570b208f7d8
CHMFC <chmfc@cin.ufpe.br>
2025-04-07 13:22:42 -0300

    Configurando a Estrutura Final

--- src/main/java/com/GenCin/GenCin/Aula/AulaRepository.java
+++ src/main/java/com/GenCin/GenCin/Aula/AulaRepository.java
@@ -1,9 +1,12 @@
 package com.GenCin.GenCin.Aula;
 
+import com.GenCin.GenCin.Professor.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
 public interface AulaRepository extends JpaRepository<Aula, UUID> {
     List<Aula> findByCodAula(String codAula);
+    List<Aula> findByProfessor(Professor professor);
 }
+
